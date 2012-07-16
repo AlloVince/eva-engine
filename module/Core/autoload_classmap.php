06d44c5ea01fe08755e4bc6af80c6665de75fd81
AlloVince <allo.vince@gmail.com>
2012-07-16 15:03:58 +0800

    Removed classmap

--- module/Core/autoload_classmap.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-/*
-    'Core\Controller\IndexController' => __DIR__ . '/src/Core/Controller/IndexController.php',
-    'Core\Controller\ErrorController' => __DIR__ . '/src/Core/Controller/ErrorController.php',
-    'Core\Module'                     => __DIR__ . '/Module.php',
- */
-);
