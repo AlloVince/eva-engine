fcb2b996210f387949367557c4a600ed97c762b9
AlloVince <allo.vince@gmail.com>
2012-07-24 23:29:06 +0800

    Code indent helper

--- module/Scaffold/config/module.config.php
+++ module/Scaffold/config/module.config.php
@@ -1,3 +1,8 @@
 <?php
 return array(
+    'view_helpers' => array(
+        'invokables' => array(
+            'codeIndent' => 'Scaffold\Helper\CodeIndent',
+        ),  
+    ),
 );
---
+++ module/Scaffold/src/Scaffold/Helper/CodeIndent.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Scaffold\Helper;
+
+use Eva\Api;
+
+/**
+ * View helper for rendering Form objects
+ * 
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class CodeIndent extends \Zend\Form\View\Helper\AbstractHelper
+{
+    /**
+     * Invoke helper as functor
+     *
+     * Proxies to {@link render()}.
+     * 
+     * @param  ElementInterface $element 
+     * @return string
+     */
+    public function __invoke($code, $indent = 4, $indentFirstLine = false)
+    {
+        $code = explode("\n", $code);
+        foreach($code as $i => $line){
+            if($i === 0 && $indentFirstLine === false){
+                continue;
+            }
+            $code[$i] = str_repeat(" ", $indent) . $line;
+        }
+        $code = implode("\n", $code);
+        return $code;
+    }
+}
