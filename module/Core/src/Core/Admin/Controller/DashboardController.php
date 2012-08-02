a1dcc7575a57f14afd9f8d2748471e1977518e54
AlloVince <allo.vince@gmail.com>
2012-08-02 13:37:51 +0800

    dashboard

--- module/Core/src/Core/Admin/Controller/DashboardController.php
+++ module/Core/src/Core/Admin/Controller/DashboardController.php
@@ -1,12 +1,32 @@
 <?php
 namespace Core\Admin\Controller;
 
-use Eva\Mvc\Controller\ActionController,
+use Eva\Api,
+    Eva\Mvc\Controller\ActionController,
     Eva\View\Model\ViewModel;
 
 class DashboardController extends ActionController
 {
     public function indexAction()
     {
+        $api = Api::_();
+        $view = array();
+        if($api->isModuleLoaded('Blog')){
+            $postModel = Api::_()->getModel('Blog\Model\Post');
+            $posts = $postModel->getPosts();
+            $postsCount = $postModel->getItemTable()->find('count');
+            $view['posts'] = $posts;
+            $view['postsCount'] = $postsCount;
+        }
+
+        if($api->isModuleLoaded('File')){
+            $fileModel = Api::_()->getModel('File\Model\File');
+            $files = $fileModel->getFiles();
+            $filesCount = $fileModel->getItemTable()->find('count');
+            $view['files'] = $files;
+            $view['filesCount'] = $filesCount;
+        }
+
+        return new ViewModel($view); 
     }
 }
