57d338c17d3364c231262868c6f3299c92a34ca8
AlloVince <allo.vince@gmail.com>
2012-08-09 16:04:54 +0800

    update image componts

--- public/static/thumb/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'sourceRootPath' => __DIR__ . '/../upload',
-    'thumbFileRootPath' => __DIR__ ,
-    'thumbUrlRootPath' => __DIR__ . '/..',
-    'saveImage' => false,
-);
