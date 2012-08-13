de2ec8169ff69307ea38945de1f49fd3edb79be1
AlloVince <allo.vince@gmail.com>
2012-08-13 17:29:13 +0800

    remove eva/core

--- vendor/Eva/Core/Exception/RestfulException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Eva\Core\Exception;
-
-class RestfulException
-    extends \RuntimeException 
-    implements ExceptionInterface
-{
-}
--- vendor/Eva/Core/Exception/RuntimeException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Eva\Core\Exception;
-
-class RuntimeException
-    extends \RuntimeException 
-    implements ExceptionInterface
-{
-}
