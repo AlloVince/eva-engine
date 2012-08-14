0e46b6535b6a95403caf707374dbd4030f986906
AlloVince <allo.vince@gmail.com>
2012-08-14 10:06:54 +0800

    update comment

--- module/Core/src/Core/Auth.php
+++ module/Core/src/Core/Auth.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * EvaEngine
  *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @link      https://github.com/AlloVince/eva-engine
+ * @copyright Copyright (c) 2012 AlloVince (http://avnpc.com/)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Session
+ * @author    AlloVince
  */
 
 namespace Core;
@@ -16,12 +16,11 @@ use Zend\Config\Config;
 use Zend\Di\Di;
 use Zend\Di\Config as DiConfig;
 
-
 /**
- * Session ManagerInterface implementation utilizing ext/session
+ * Core Authentication
  *
- * @category   Zend
- * @package    Zend_Session
+ * @category   Core
+ * @package    Core_Auth
  */
 class Auth
 {
