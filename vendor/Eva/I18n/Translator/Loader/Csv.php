2d94b8635c20b3e9e4d1ce2b263415ab7e2f38fe
ctqh <ctqh@easthv.com>
2012-08-14 17:34:05 +0800

    csv loader fix

--- vendor/Eva/I18n/Translator/Loader/Csv.php
+++ vendor/Eva/I18n/Translator/Loader/Csv.php
@@ -44,7 +44,7 @@ use Zend\I18n\Translator\TextDomain;
 
         $messages = array();
         if (($handle = fopen($filename, "r")) !== false) {
-            while (($cell = fgetcsv($handle)) !== false) {
+            while (($cell = $this->fgetcsv($handle)) !== false) {
                 if(!isset($cell[0])){
                     continue;
                 }
@@ -66,5 +66,28 @@ use Zend\I18n\Translator\TextDomain;
         }
 
         return $textDomain;
+    }
+
+    protected function fgetcsv(& $handle, $length = null, $d = ',', $e = '"') 
+    {
+        $d = preg_quote($d);
+        $e = preg_quote($e);
+        $_line = "";
+        $eof=false;
+        while ($eof != true) {
+            $_line .= (empty ($length) ? fgets($handle) : fgets($handle, $length));
+            $itemcnt = preg_match_all('/' . $e . '/', $_line, $dummy);
+            if ($itemcnt % 2 == 0)
+                $eof = true;
+        }
+        $_csv_line = preg_replace('/(?: |[ ])?$/', $d, trim($_line));
+        $_csv_pattern = '/(' . $e . '[^' . $e . ']*(?:' . $e . $e . '[^' . $e . ']*)*' . $e . '|[^' . $d . ']*)' . $d . '/';
+        preg_match_all($_csv_pattern, $_csv_line, $_csv_matches);
+        $_csv_data = $_csv_matches[1];
+        for ($_csv_i = 0; $_csv_i < count($_csv_data); $_csv_i++) {
+            $_csv_data[$_csv_i] = preg_replace('/^' . $e . '(.*)' . $e . '$/s', '$1' , $_csv_data[$_csv_i]);
+            $_csv_data[$_csv_i] = str_replace($e . $e, $e, $_csv_data[$_csv_i]);
+        }
+        return empty ($_line) ? false : $_csv_data;
     }
 }
