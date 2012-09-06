50245eae032c8f9e89c03fd49aafb2c41b46a3e3
AlloVince <allo.vince@gmail.com>
2012-09-06 10:31:02 +0800

    not empty validation message

--- module/Blog/src/Blog/Form/PostForm.php
+++ module/Blog/src/Blog/Form/PostForm.php
@@ -220,6 +220,9 @@ class PostForm extends Form
                 'notEmpty' => array (
                     'name' => 'NotEmpty',
                     'options' => array (
+                        'messages' => array(
+                            \Zend\Validator\NotEmpty::IS_EMPTY => 'Title not allow empty',
+                        ), 
                     ),
                 ),
                 'stringLength' => array (
