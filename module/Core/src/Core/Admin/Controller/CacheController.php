8a1fed6e4934b8159687538841d264f572c70810
ctqh <ctqh@easthv.com>
2012-09-28 16:54:12 +0800

    memcached clean

--- module/Core/src/Core/Admin/Controller/CacheController.php
+++ module/Core/src/Core/Admin/Controller/CacheController.php
@@ -10,11 +10,45 @@ use Zend\Stdlib\ErrorHandler;
 class CacheController extends RestfulModuleController
 {
     protected $addResources = array(
-        'static'
+        'static',
+        'memcached',
     );
 
     public function restIndexCache()
     {
+        $config = $this->getEvent()->getApplication()->getConfig();
+        
+        if($config['cache']['page_capture']['adapter'] == 'memcached'){
+            $config = $config['cache']['page_capture'];
+
+            $stats = $this->getStats($config);
+
+            if ($stats) {
+                foreach ($stats as $key=>$server) {
+                    $stats[$key]['totalSpace'] = $server['limit_maxbytes'];
+                    $stats[$key]['availableSpace'] = $server['limit_maxbytes'] - $server['bytes'];
+                    $stats[$key]['hitRate'] = round($server['get_hits']/$server['cmd_get'] * 100, 2);
+                }
+            }
+
+            return array(
+                'stats' => $stats,
+            );
+        }    
+    }
+    
+    public function getStats($config)
+    {
+        if (!$config) {
+            return array();
+        }
+    
+        $memcached = new \Memcached();
+        $memcached->setOption($memcached::OPT_PREFIX_KEY, $config['options']['namespace']);
+        $memcached->addServers($config['options']['servers']);
+        $stats = $memcached->getStats();
+        
+        return $stats;
     }
 
     /**
@@ -31,7 +65,7 @@ class CacheController extends RestfulModuleController
         ErrorHandler::start();
         for($i = 0; $i < $level; $i++){
             $path = $rootPath . str_repeat(\DIRECTORY_SEPARATOR . '*', $i)
-            . \DIRECTORY_SEPARATOR . '*' . '.' . $ext;
+                . \DIRECTORY_SEPARATOR . '*' . '.' . $ext;
             $glob = new GlobIterator($path, $flags);
             foreach ($glob as $pathname) {
                 $files[] = $pathname;
@@ -46,6 +80,25 @@ class CacheController extends RestfulModuleController
         return $files;
     }
 
+    /**
+     * Flush cache
+     *
+     * @param string $namespace
+     * @return boolean
+     */
+    protected function flush($config)
+    {
+        $options = $config['options'];
+        unset($options['public_dir']);
+        $cache = new \Zend\Cache\Storage\Adapter\Memcached($options); 
+        $totalSpace = $cache->getTotalSpace();
+        $availableSpace = $cache->getAvailableSpace(); 
+    
+        $cache->flush();
+    
+        return ($totalSpace - $availableSpace);
+    }
+
     public function restGetCacheStatic()
     {
         $config = $this->getEvent()->getApplication()->getConfig();
@@ -58,5 +111,20 @@ class CacheController extends RestfulModuleController
         $files = $this->clear($cachePath, $cacheExt);
 
         return array('files' => $files);
+    }
+
+    public function restGetCacheMemcached()
+    {
+        $config = $this->getEvent()->getApplication()->getConfig();
+
+        if(!isset($config['cache']['page_capture'])){
+            $this->flashMessenger()->addMessage('no-cache-found');
+            return $this->redirect()->toUrl('/admin/core/cache/');
+        }
+
+        $config = $config['cache']['page_capture'];
+        $releaseSpace = $this->flush($config);
+
+        return array('releaseSpace' => $releaseSpace);
     }
 }
