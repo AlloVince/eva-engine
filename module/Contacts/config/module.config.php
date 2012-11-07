355d7b215ef6ee02ecbb642aa683c4c5da24366b
ctqh <ctqh@easthv.com>
2012-11-07 14:39:14 +0800

    contacts config

--- module/Contacts/config/module.config.php
+++ module/Contacts/config/module.config.php
@@ -3,4 +3,17 @@ return array(
     'contacts' => array(
         'import_url_path' => '/contacts/import/',
     ),
+    'cache' => array(
+        'contacts_import' => array(
+            'enable' => 1,
+            'adapter' => array(
+                'name' => 'filesystem',
+                'options' => array(
+                    'cacheDir' => EVA_ROOT_PATH . '/data/cache/other/',
+                    'ttl' => 6000,
+                ),
+            ),
+            'plugins' => array('serializer')
+        ),
+    ),
 );
