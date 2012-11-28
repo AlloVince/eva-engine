dfa37d62eba56545a3fb8204a0caec4e5b67b1b6
AlloVince <allo.vince@gmail.com>
2012-11-28 15:43:35 +0800

    fixed google scope

--- module/Oauth/src/Oauth/Adapter/Oauth2/Google.php
+++ module/Oauth/src/Oauth/Adapter/Oauth2/Google.php
@@ -4,6 +4,7 @@ namespace Oauth\Adapter\Oauth2;
 
 use Oauth\Adapter\Oauth2\AbstractAdapter;
 use ZendOAuth\OAuth;
+use Oauth\Service\Token\Access as AccessToken;
 
 class Google extends AbstractAdapter
 {
@@ -12,6 +13,42 @@ class Google extends AbstractAdapter
 
     protected $defaultOptions = array(
         'requestScheme' => OAuth::REQUEST_SCHEME_POSTBODY,
-        'scope' => 'https://www.google.com/m8/feeds/',
+        'scope' => 'https://www.googleapis.com/auth/userinfo.profile',
     );
+
+    protected $httpClientOptions = array(
+        'callback' => 'getAccessTokenClient',
+    );
+
+    public function getAccessTokenClient($client)
+    {
+        $accessToken = $client->getToken();
+        if(!$accessToken){
+            throw new Exception\InvalidArgumentException(sprintf('No access token found'));
+        }
+
+        $accessTokenString = $accessToken->getParam('access_token');
+        $client->setHeaders(array("Authorization: OAuth $accessTokenString"));
+        return $client;
+    }
+
+    public function accessTokenToArray(AccessToken $accessToken)
+    {
+        $token = parent::accessTokenToArray($accessToken);
+        if(!isset($token['remoteUserId']) || !$token['remoteUserId']){
+            $token['remoteUserId'] = $this->getRemoteUserId();
+        }
+        return $token;
+    }
+
+    public function getRemoteUserId()
+    {
+        $client = $this->getHttpClient();
+        $client->setUri('https://www.googleapis.com/oauth2/v2/userinfo');
+        $response = $client->send();
+        if($response->getStatusCode() == 200){
+            $data = (array) \Zend\Json\Json::decode($response->getBody());
+            return $data['id'];
+        }
+    }
 }
