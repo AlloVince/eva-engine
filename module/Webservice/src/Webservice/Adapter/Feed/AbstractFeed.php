3b49c8f0d5b8f15fab9f894c83d41bab558cc8bf
AlloVince <allo.vince@gmail.com>
2012-11-30 18:06:48 +0800

    add twitter feed

---
+++ module/Webservice/src/Webservice/Adapter/Feed/AbstractFeed.php
@@ -0,0 +1,43 @@
+<?php
+    
+namespace Webservice\Adapter\Feed;
+
+use Webservice\Adapter\AbstractUniform;
+use Webservice\Exception;
+
+abstract class AbstractFeed extends AbstractUniform implements UserInterface
+{
+
+    protected $feedId;
+
+    public function setFeedId($feedId)
+    {
+        $this->feedId = $feedId;
+        return $this;
+    }
+
+    public function getFeed($feedId = null)
+    {
+        return $this->getData('Feed');
+    }
+
+    public function getFeedList($start, $rows = 10)
+    {
+        return $this->getData('FeedList');
+    }
+
+    public function createFeed($content)
+    {
+    
+    }
+
+    public function createFeedWithPic($content, $pic)
+    {
+    }
+
+    public function removeFeed($feedId)
+    {
+    
+    }
+
+}
