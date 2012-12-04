16c897a28564cd3fd4d330b974a9f8763a6ae51c
AlloVince <allo.vince@gmail.com>
2012-12-04 15:14:47 +0800

    Added disqus

--- module/Oauth/config/module.config.php
+++ module/Oauth/config/module.config.php
@@ -14,37 +14,37 @@ return array(
         'login_url_path' => '/',
         'oauth1' => array(
             'twitter' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'douban' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'weibo' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => '',
             ),
             'flickr' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'dropbox' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'linkedin' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'yahoo' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
@@ -52,87 +52,92 @@ return array(
         ),
         'oauth2' => array(
             'facebook' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'weibo' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'baidu' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'douban' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'qihoo' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'taobao' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'tencent' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'qzone' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'renren' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'tqq' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'kaixin' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'pengyou' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'qplus' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'msn' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'google' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'github' => array(
-                'enable' => 1,
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
             'foursquare' => array(
-                'enable' => 1,
+                'enable' => 0,
+                'consumer_key' => '',
+                'consumer_secret' => ''
+            ),
+            'disqus' => array(
+                'enable' => 0,
                 'consumer_key' => '',
                 'consumer_secret' => ''
             ),
