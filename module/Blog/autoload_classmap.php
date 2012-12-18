117cf35990b1794f97e9d9d4bd884f7eb573a354
AlloVince <allo.vince@gmail.com>
2012-12-18 11:42:39 +0800

    collect all 3rd party libs into one

---
+++ module/Blog/autoload_classmap.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    'Markdown_Parser' => EVA_LIB_PATH . '/Libs/Markdown/Markdown_Parser.php',
+    'MarkdownExtra_Parser' => EVA_LIB_PATH . '/Libs/Markdown/MarkdownExtra_Parser.php',
+);
