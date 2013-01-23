8d0e8503935ab934b35d8d30784712817e3e3fdd
AlloVince <allovince@allovince-ThinkPad-T420s.(none)>
2013-01-23 15:10:31 +0800

    monitor ajax view

--- module/Core/src/Core/Admin/Controller/QueueController.php
+++ module/Core/src/Core/Admin/Controller/QueueController.php
@@ -5,6 +5,7 @@ namespace Core\Admin\Controller;
 use Eva\Mvc\Controller\RestfulModuleController;
 use Eva\View\Model\ViewModel;
 use Zend\View\Model\JsonModel;
+use Eva\Api;
 
 class QueueController extends RestfulModuleController
 {
@@ -20,7 +21,10 @@ class QueueController extends RestfulModuleController
 
     public function restIndexQueue()
     {
-        $queues = \Resque::queues();
+        $config = Api::_()->getConfig();
+        $defaultQueues = $config['queue']['default_queues'];
+        $queues = array_merge($defaultQueues, \Resque::queues());
+        $queues = array_unique($queues);
         foreach($queues as $key => $queue){
             $queues[$key] = array(
                 'name' => $queue,
@@ -28,9 +32,32 @@ class QueueController extends RestfulModuleController
             );
         }
 
-        return array(
+        $pslist = $this->getSystemProcessList('ps ux | grep "php ' . realpath(EVA_ROOT_PATH . '/workers') . '" | grep -v grep');
+        foreach($queues as $key => $queue){
+            $workerCount = 0;
+            foreach($pslist as $psKey => $ps){
+                $offset = strlen($ps['COMMAND']) - strlen($queue['name']);
+                if($offset === strrpos($ps['COMMAND'], $queue['name'])){
+                    $workerCount++;
+                    unset($pslist[$psKey]);
+                }
+            }
+            $queues[$key]['workers'] = $workerCount;
+        }
+        $publicWorkerCount = count($pslist);
+
+        $return = array(
             'items' => $queues,
+            'publicWorkerCount' => $publicWorkerCount,
         );
+
+
+        if($this->params()->fromQuery('format') == 'json'){
+            $this->changeViewModel('json');
+            return new JsonModel($return);
+        }
+
+        return $return;
     }
 
     public function restGetQueueView()
@@ -44,7 +71,8 @@ class QueueController extends RestfulModuleController
         $queue = $this->params()->fromQuery('queue', 'default');
         $jobId = \Resque::enqueue($queue, 'Core\Jobs\TestJob', array('name' => $queue), true);
         return new JsonModel(array(
-            'id' => $jobId
+            'id' => $jobId,
+            'size' => \Resque::size($queue), 
         ));
     }
 
@@ -60,11 +88,16 @@ class QueueController extends RestfulModuleController
 
     public function restGetQueueRun()
     {
+        $queue = $this->params()->fromQuery('queue');
         $this->changeViewModel('json');
         $worker = $this->params()->fromQuery('worker', 'all');
         $path = realpath(EVA_ROOT_PATH . '/workers/worker');
-        $command = "QUEUE=* php $path &";
-        $out = popen("QUEUE=* php $path &", "r"); 
+        if($queue){
+            $command = "QUEUE=$queue php $path $queue &";
+        } else {
+            $command = "QUEUE=* php $path &";
+        }
+        $out = popen($command, "r"); 
         pclose($out);
         return new JsonModel(array(
             'command' => $command
@@ -80,56 +113,23 @@ class QueueController extends RestfulModuleController
         ));
     }
 
+
+
     public function restGetQueueMonitor()
     {
         if($this->params()->fromQuery('format') == 'json'){
             $this->changeViewModel('json');
-            $pslist = array();
-            $psData = array();
-            $queue = $this->params()->fromQuery('queue');
+            $queue = $this->params('id');
             $command = 'ps ux';
-            if($queue){
-                $command .= ' | grep "php ' . realpath(EVA_ROOT_PATH . '/workers') . '/worker" | grep -v grep';
+            if($queue != 'monitor'){
+                $command .= ' | grep "php ' . realpath(EVA_ROOT_PATH . '/workers/worker') . ' ' . $queue . '" | grep -v grep';
             } else {
-                $command .= ' | grep "php ' . realpath(EVA_ROOT_PATH . '/workers') . '" | grep -v grep';
+                $command .= ' | grep "php ' . realpath(EVA_ROOT_PATH . '/workers/worker') . '" | grep -v grep';
             }
-            exec($command, $pslist);
-            $psCount = count($pslist);
-            if($psCount <= 0){
-                return new JsonModel(array(
-                ));
-            }
-
-            $column = array(
-                'USER',
-                'PID',
-                'CPU',
-                'MEM',
-                'VSZ',
-                'RSS',
-                'TTY',
-                'STAT',
-                'START',
-                'TIME',
-                'COMMAND',
-            );
-
-            for($i = 0; $i < $psCount; $i++) {
-                $item = array();
-                $process = $pslist[$i];
-                if($i === 0 && 0 === strpos($process, 'USER')){
-                    continue;
-                }
-                $item[0] = strtok($process, " "); 
-                for($s = 1; $s < 11; $s++) { 
-                    $item[$s]  = strtok(" "); 
-                } 
-                $psData[] = @array_combine($column, $item);
-            } 
-            $psData = \Eva\Stdlib\Arraylib\Sort::multiSortArray($psData, 'CPU', 'SORT_DESC');
+            $pslist = $this->getSystemProcessList($command);
             return new JsonModel(array(
                 'command' => $command,
-                'items' => $psData,
+                'items' => $pslist,
             ));
         }
         return array();
@@ -148,5 +148,55 @@ class QueueController extends RestfulModuleController
         return array(
             'status' => $status->get()
         );
+    }
+
+    protected function getQueueList()
+    {
+    
+    }
+
+    protected function getSystemProcessList($command)
+    {
+        $pslist = array();
+
+        exec($command, $pslist);
+        $psCount = count($pslist);
+        if($psCount <= 0){
+            return array();
+        }
+
+        $psData = array();
+        $column = array(
+            'USER',
+            'PID',
+            'CPU',
+            'MEM',
+            'VSZ',
+            'RSS',
+            'TTY',
+            'STAT',
+            'START',
+            'TIME',
+            'COMMAND',
+        );
+
+        for($i = 0; $i < $psCount; $i++) {
+            $item = array();
+            $process = $pslist[$i];
+            if($i === 0 && 0 === strpos($process, 'USER')){
+                continue;
+            }
+            $token = ' ';
+            for($s = 0; $s < 10; $s++) {
+                $tokenIndex = strpos($process, $token);
+                $text = substr($process, 0, $tokenIndex);
+                $process = ltrim(substr($process, $tokenIndex));
+                $item[$s] = $text;
+            } 
+            $item[10] = rtrim(substr($process, $tokenIndex));
+            $psData[] = array_combine($column, $item);
+        } 
+        $psData = \Eva\Stdlib\Arraylib\Sort::multiSortArray($psData, 'CPU', 'SORT_DESC');
+        return $psData;
     }
 }
