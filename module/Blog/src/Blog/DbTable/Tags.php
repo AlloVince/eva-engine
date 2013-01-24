f2c4149a945504d90cb05447f6cfc02bf7d073c7
AlloVince <allo.vince@gmail.com>
2013-01-24 12:10:56 +0800

    tag count order

--- module/Blog/src/Blog/DbTable/Tags.php
+++ module/Blog/src/Blog/DbTable/Tags.php
@@ -4,6 +4,8 @@ namespace Blog\DbTable;
 
 use Eva\Db\TableGateway\TableGateway;
 use Zend\Stdlib\Parameters;
+use Zend\Db\Sql\Expression;
+use Eva\Api;
 
 class Tags extends TableGateway
 {
@@ -25,11 +27,32 @@ class Tags extends TableGateway
             $this->disableLimit();
         }
 
+        if ($params->rows) {
+            $this->limit((int) $params->rows);
+        }
+
+        if ($params->order == 'postcountdesc' || $params->order == 'postcountasc') {
+            $postTagTable = Api::_()->getDbTable('Blog\DbTable\TagsPosts');
+            $postTagTableName = $postTagTable->initTableName()->getTable();
+
+            $this->join(
+                $postTagTableName,
+                "id = $postTagTableName.tag_id"
+            );
+            $this->columns(array(
+                '*',
+                'PostCount' => new Expression("count(post_id)"),
+            ));
+            $this->group('tag_id');
+        }
+
         $orders = array(
             'idasc' => 'id ASC',
             'iddesc' => 'id DESC',
             'nameasc' => 'tagName ASC',
             'namedesc' => 'tagName DESC',
+            'postcountasc' => 'PostCount ASC',
+            'postcountdesc' => 'PostCount DESC',
         );
 
         if($params->order){
