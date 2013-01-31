262269f5011fdc3f9e4610925496f062a47ec73b
AlloVince <allo.vince@gmail.com>
2013-01-31 15:59:53 +0800

    update eva base service

--- module/Core/config/module.config.php
+++ module/Core/config/module.config.php
@@ -61,7 +61,7 @@ return array(
     'controllers' => array(
         'invokables' => array(
             'ErrorController' => 'Core\Controller\ErrorController',
-            'QueueController' => 'Core\Controller\QueueController',
+
         ),
     ),
 
