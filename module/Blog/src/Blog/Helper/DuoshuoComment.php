13bff0d3cb29d993603d77289ce9bfefe212d377
AlloVince <allo.vince@gmail.com>
2015-12-31 13:45:46 +0800

    Update DuoshuoComment.php

--- module/Blog/src/Blog/Helper/DuoshuoComment.php
+++ module/Blog/src/Blog/Helper/DuoshuoComment.php
@@ -76,7 +76,7 @@ class DuoshuoComment extends \Zend\View\Helper\AbstractHelper
             (function() {
                 var ds = document.createElement('script');
                 ds.type = 'text/javascript';ds.async = true;
-                ds.src = 'http://static.duoshuo.com/embed.js';
+                ds.src = 'https://static.duoshuo.com/embed.js';
                 ds.charset = 'UTF-8';
                 (document.getElementsByTagName('head')[0] 
                 || document.getElementsByTagName('body')[0]).appendChild(ds);
